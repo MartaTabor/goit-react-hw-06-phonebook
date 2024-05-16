7d2938ef1df73e96bf474f5b4efe386c53e6e1dc
Marta Tabor <leeloothefifth@googlemail.com>
2024-05-16 21:18:06 +0100

    slice, store and app add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,74 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+import { useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { SearchBar } from './SearchBar/SearchBar';
 import ContactsList from './ContactList/ContactsList';
 import css from './App.module.css';
-
-const INITIAL_STATE = {
-  contacts: [],
-  filter: '',
-  name: '',
-  number: '',
-};
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  addContact,
+  deleteContact,
+  setName,
+  setNumber,
+  setFilter,
+} from '../redux/contactsSlice';
 
 export const App = () => {
-  const [userData, setUserData] = useState(() => {
-    const savedUserData = localStorage.getItem('user-data');
-    return savedUserData ? JSON.parse(savedUserData) : INITIAL_STATE;
-  });
+  const dispatch = useDispatch();
+  const { contacts, filter, name, number } = useSelector(
+    (state = state.contacts)
+  );
 
   useEffect(() => {
-    localStorage.setItem('user-data', JSON.stringify(userData));
-  }, [userData]);
+    const savedUserData = localStorage.getItem('user-data');
+    if (savedUserData) {
+      const data = JSON.parse(savedUserData);
+      dispatch(setName(data.name));
+      dispatch(setNumber(data.number));
+      data.contacts.forEach(contact => dispatch(addContact(contact)));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      'user-data',
+      JSON.stringify({ contacts, filter, name, number })
+    );
+  }, [contacts, filter, name, number]);
 
   const onChange = event => {
-    setUserData({ ...userData, [event.target.name]: event.target.value });
+    const { name, value } = event.target;
+    if (name === 'name') {
+      dispatch(setName(value));
+    } else if (name === 'number') {
+      dispatch(setNumber(value));
+    } else if (name === 'filter') {
+      dispatch(setFilter(value));
+    }
   };
 
   const onSubmit = event => {
     event.preventDefault();
-
-    const { name, number, contacts } = userData;
-
-    const isExist = contacts.some(
-      contact => contact.name.toLowerCase() === name.trim().toLowerCase()
-    );
-    if (isExist) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-
-    setUserData({
-      ...userData,
-      contacts: [...contacts, { name, number, id: nanoid() }],
-      name: '',
-      number: '',
-    });
+    dispatch(addContact({ name, number }));
   };
 
   const handleDelete = id => {
-    setUserData({
-      ...userData,
-      contacts: userData.contacts.filter(contact => contact.id !== id),
-    });
-  };
-
-  const handleFilterChange = event => {
-    setUserData({ ...userData, filter: event.target.value });
+    dispatch(deleteContact(id));
   };
 
   return (
     <div className={css.container}>
       <h1 className={css.heading}>Phonebook</h1>
       <ContactForm
-        userData={userData}
+        name={name}
+        number={number}
         onChange={onChange}
         onSubmit={onSubmit}
       />
-      <SearchBar filter={userData.filter} onFilterChange={handleFilterChange} />
+      <SearchBar filter={filter} onFilterChange={onChange} />
       <h2 className={css.heading}>Contacts</h2>
       <ContactsList
-        contacts={userData.contacts}
-        filter={userData.filter}
+        contacts={contacts}
+        filter={filter}
         onDelete={handleDelete}
       />
     </div>
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const initialState = {
+  contacts: [],
+  filter: '',
+  name: '',
+  number: '',
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      const { name, number } = action.payload;
+      const isExist = state.contacts.some(
+        contact => contact.name.toLowerCase() === name.trim().toLowerCase()
+      );
+      if (isExist) {
+        alert(`&{name} is already in contacts`);
+      } else {
+        state.contacts.push({ id: nanoid(), name, number });
+        state.name = '';
+        state.number = '';
+      }
+    },
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+    setName: (state, action) => {
+      state.name = action.payload;
+    },
+    setNumber: (state, action) => {
+      state.number = action.payload;
+    },
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { addContact, deleteContact, setName, setNumber, setFilter } =
+  contactsSlice.actions;
+export default contactsSlice.reducer;
